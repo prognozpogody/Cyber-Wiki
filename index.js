00ca0701e772d9a829649872638f119b80911d4e
gennadiy <prognozpogody@bk.ru>
2022-12-16 18:41:03 +0300

    change color

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
 const $wrapper = document.querySelector("[data-wrapper]");
-console.log($wrapper);
+const $addButton = document.querySelector('[data-action="addButton"]');
+const $modal = document.querySelector("[data-modal]");
 
 const genCat = (
   cat
-) => `<div data-card_id="${cat.id}" class="card" style="width: 18rem;">
+) => `<div data-card_id="${cat.id}" class="card m-3 animate__animated animate__fadeIn" style="width: 18rem;">
 <img src="${cat.image}" style="width: 286px; height: 300px; object-fit: cover;" alt="${cat.name}">
-<div class="card-body">
-  <h5 class="card-title">${cat.name}</h5>
-  <p class="card-text">${cat.description}</p>
+<div class="card-body text-bg-danger ">
+  <h3 class="card-title">${cat.name}</h3>
+  <div class="d-flex justify-content-sm-evenly">
   <button data-action="show" class="btn btn-primary">Show</button>
   <button data-action="delete" class="btn btn-danger">Delete</button>
+  </div>
 </div>
 </div>`;
 
 $wrapper.addEventListener("click", (event) => {
   switch (event.target.dataset.action) {
     case "delete":
-      const $currentCard = event.target.closest("[data-card_id]");
-      const catId = $currentCard.dataset.card_id;
-      api.delCat(catId);
-      $currentCard.remove();
+      // Функционал удаление, убран от похотливых рук
+      //_______________________________________________
+      // const $currentCard = event.target.closest("[data-card_id]");
+      // const catId = $currentCard.dataset.card_id;
+      // api.delCat(catId);
+      // $currentCard.remove();
+      alert("Не надо портить прекрасное!");
       break;
 
     default:
@@ -29,17 +34,19 @@ $wrapper.addEventListener("click", (event) => {
 
 document.forms.catsForm.addEventListener("submit", () => {
   event.preventDefault();
+  console.log(event);
   const data = Object.fromEntries(new FormData(event.target).entries());
-
-  data.name;
-  data.idNumber = Number(data.idNumber);
-  data.urlCat;
-  data.ageCat = Number(data.ageCat);
-  data.rating = Number(data.rating);
+  data.id = Number(data.id);
+  data.age = Number(data.age);
+  data.rate = Number(data.rate);
   data.favorite = data.favorite === "on";
-  data.description;
-  
-  api.addCat(data).then(Response)
+  api
+    .addCat(data)
+    .then((Response) => Response.ok && $modal.classList.add("hidden"));
+});
+
+$addButton.addEventListener("click", () => {
+  $modal.classList.remove("hidden");
 });
 
 const api = new Api("kg");
